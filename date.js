f6aca3b611c1a6ef24aba55907ae1f9111f296da
d21-12 <kimbuun1@kimbuun1gmail.com>
2024-10-10 13:48:21 +0700

    Xóa toàn bộ nội dung của repository

--- date.js
+++
@@ -1,4 +0,0 @@
-const myDateTime = (str="Ngày hiện tại:") =>{
-    return str + Date()
-}
-moduledefaut = myDateTime
--- getURL.js
+++
@@ -1,8 +0,0 @@
-var url = import('url')
-const getPath = (req) =>{
-    return req.url;
-}
-const getParamsURL = (req) =>{
-    return JSON.stringify((url.parse(req.url, true)).query);
-}
-moduledefaut ={getPath,getParamsURL}
